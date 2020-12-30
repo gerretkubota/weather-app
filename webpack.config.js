16ef3d5d5ed4285982f40fc14ffad49568e195b2
Gerret Mozii <gerretkubota@gmail.com>
2020-12-29 22:36:41 -0800

    configuring webpack to accept a valid API key for this app when running through the command line

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,60 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: path.resolve(__dirname, '/src/index.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: [/\.(js|jsx)$/],
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-      {
-        resolve: {
-          extensions: ['.js', '.jsx'],
+module.exports = (env) => {
+  if (!env || !env.API_KEY) {
+    throw new Error(
+      'Make sure to run the command as npm run:devserver -- --env API_KEY=<yourApiKeyHere>'
+    );
+  }
+
+  return {
+    entry: path.resolve(__dirname, '/src/index.js'),
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.(js|jsx)$/],
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+          ],
         },
-      },
-      {
-        test: [/\.(sa|sc|c)ss$/],
-        use: ['style-loader', 'css-loader'],
-      },
+        {
+          resolve: {
+            extensions: ['.js', '.jsx'],
+          },
+        },
+        {
+          test: [/\.(sa|sc|c)ss$/],
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'template/index.html'),
+        filename: 'index.html',
+        inject: true,
+      }),
+      new webpack.DefinePlugin({
+        'process.env.API_KEY': JSON.stringify((env && env.API_KEY) || ''),
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'template/index.html'),
-      filename: 'index.html',
-      inject: true,
-    }),
-  ],
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    hot: true,
-    compress: true,
-    open: true,
-    inline: true,
-  },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      hot: true,
+      compress: true,
+      open: true,
+      inline: true,
+    },
+  };
 };
